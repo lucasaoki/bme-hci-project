0a8e15f78c860e87a1f284eb3c356cf6de8df02c
lucasaoki <lucas.heredia@usp.br>
2014-11-15 16:11:43 +0100

    ...

--- Eleven/Eleven/Properties/AssemblyInfo.cs
+++ Eleven/Eleven/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Eleven")]
+[assembly: AssemblyTitle("bme-hci-project")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Eleven")]
+[assembly: AssemblyProduct("bme-hci-project")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
